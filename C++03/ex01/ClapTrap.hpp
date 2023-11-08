783fc5934b6fb36f5dd57374f9e454bb3f2fd9e7
José Gonçalves <118729517+ZPedro99@users.noreply.github.com>
2023-11-08 16:13:18 +0000

    Add files via upload

---
+++ C++03/ex01/ClapTrap.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jomirand <jomirand@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 14:07:13 by jomirand          #+#    #+#             */
+/*   Updated: 2023/11/08 15:34:58 by jomirand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <iostream>
+#include "ScavTrap.hpp"
+
+class ClapTrap
+{
+	private:
+		std::string	_name;
+		int			_hitPoints;
+		int			_energyPoints;
+		int			_attackDamage;
+	public:
+		ClapTrap();
+		ClapTrap(std::string name);
+		ClapTrap(const ClapTrap &obj);
+		ClapTrap&	operator=(const ClapTrap &obj);
+		~ClapTrap();
+		void		attack(const std::string& target);
+		void		takeDamage(unsigned int amount);
+		void		beRepaired(unsigned int amount);
+};
+
+#endif
